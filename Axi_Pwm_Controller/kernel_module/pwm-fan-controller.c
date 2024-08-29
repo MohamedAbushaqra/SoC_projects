e552fd1e38dab66fb669bdfa69d1146110223df1
MohamedAbushaqra <mabushaqraedu0@gmail.com>
2024-08-29 21:10:22 +0200

    Add the pwm fan controller linux driver, add percentage mapping

--- Axi_Pwm_Controller/kernel_module/pwm-fan-controller.c
+++ Axi_Pwm_Controller/kernel_module/pwm-fan-controller.c
@@ -1,7 +1,3 @@
-/* Abushaqra - August - 20 - 2024 - created */
-/* This Kernel Level driver enables the control of the pwm capable fan ip core in Vivado */
-/* Any kind of distribution of this Kernel Level Driver without the Permission of the Author is FORBIDDEN */
-
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
